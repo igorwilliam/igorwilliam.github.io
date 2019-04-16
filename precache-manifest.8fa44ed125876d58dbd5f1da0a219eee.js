26890865db6aa37fa5f58fe8bb23c264d443dbfc
Igor William <igorwm9@gmail.com>
2019-04-16 11:56:07 -0300

    Updates

---
+++ precache-manifest.8fa44ed125876d58dbd5f1da0a219eee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ecd5ccfb5defbd26b1f5",
+    "url": "/static/js/main.e40c1658.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "ecd5ccfb5defbd26b1f5",
+    "url": "/static/css/main.49dfa80b.chunk.css"
+  },
+  {
+    "revision": "ae9372e24690079c63958316bf1da9f8",
+    "url": "/index.html"
+  }
+];

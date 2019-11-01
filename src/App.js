d86a1ed72ba2755392f7910b7b7f2534188872c7
Matheus Lacerda <the.matheus6@gmail.com>
2019-10-31 23:47:45 -0300

    style: new glitch bg and reacttored flex layout with responsive

--- src/App.js
+++ src/App.js
@@ -1,39 +1,65 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Button, Icon } from 'semantic-ui-react'
+
+
+import Glitch1 from './assets/backgroud future/glitch-2717630_1920.jpg';
+import Glitch2 from './assets/backgroud future/glitch-2717632_1920.jpg';
+import Glitch3 from './assets/backgroud future/glitch-2717633_1920.jpg';
+import Glitch4 from './assets/backgroud future/glitch-2717636_1920.jpg';
+import Glitch5 from './assets/backgroud future/glitch-2722503_1920.jpg';
+
 
 import logo from './assets/intro.gif';
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 
+const GLITCHES = [
+  Glitch1,
+  Glitch2,
+  Glitch3,
+  Glitch4,
+  Glitch5,
+];
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
+function random(min, max) {
+  const random = Math.random() * (+max - +min) + +min; 
 
-        <header className="App-header">
-        
-          <img src={logo} className="App-logo" alt="logo" />
-          
-          <a
-            className="animated infinite tada alternative"
-            href="https://www.linkedin.com/in/igorwilliam/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >      
-            <Button color='linkedin' size='huge'>
-              <Icon name='linkedin' /> LinkedIn
-           </Button>
+  return Math.floor(random);
+}
 
-          </a>
+function App() {
+  const [Bg, setBg] = React.useState(Glitch1);
 
-            <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="pt_BR" data-type="vertical" data-theme="dark" data-vanity="igorwilliam"><a class="LI-simple-link" href='https://br.linkedin.com/in/igorwilliam?trk=profile-badge'>Igor William Pessoa da Silva</a></div>    
-          
-   
-        </header>
-      </div>
-    );
-  }
+
+  React.useEffect(() => {
+    setInterval(() => {
+      setBg(GLITCHES[random(0, 4)])
+    }, random(600, 900));
+  }); 
+
+  return (
+    <div className="App" style={{ backgroundSize: 'cover', backgroundImage: `url(${Bg})` }}>
+
+      <header className="App-header">
+
+        <img src={logo} className="App-logo" alt="Igor William" />
+
+        <a
+          className="animated infinite tada alternative"
+          href="https://www.linkedin.com/in/igorwilliam/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {/* <Button color='linkedin' size='huge'>
+            <Icon name='linkedin' /> LinkedIn
+           </Button> */}
+
+        </a>
+
+        <div class="LI-profile-badge" data-version="v1" data-size="medium" data-locale="pt_BR" data-type="vertical" data-theme="dark" data-vanity="igorwilliam"><a class="LI-simple-link" href='https://br.linkedin.com/in/igorwilliam?trk=profile-badge'>Igor William Pessoa da Silva</a></div>
+      </header>
+    </div>
+  );
 }
 
 export default App;
